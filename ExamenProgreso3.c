728800fe40edafe5743bac49d143c024bc144944
Brandon Arellano <brandon.arellano@udla.edu.ec>
2023-07-10 15:05:03 -0500

    Declaracion del Main y librerias

---
+++ ExamenProgreso3.c
@@ -0,0 +1,15 @@
+/*Examen progreso 3
+Construya un programa que lea los datos de un archivo plano que contiene los datos de un conjunto de estudiantes, ordenados en función de su código, 
+y genere un nuevo archivo ordenado de mayor a menor e incluya el cálculo de la nota promedio.
+Utilice los conceptos de: Funciones, Punteros, Estructuras, Archivos.
+Hecho por Brandon Arellano
+Programacion 1
+Universidad de las Americas
+*/
+#include <stdio.h>
+
+
+int main(){
+
+    return 0;
+}
